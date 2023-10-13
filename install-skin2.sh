a949d811afaf04f25fe8c10117ff77f5573a0b05
coucouillu <31075832+coucouillu@users.noreply.github.com>
2023-10-14 00:35:59 +0200

    Update install-skin2.sh

--- install-skin2.sh
+++ install-skin2.sh
@@ -1,7 +1,7 @@
 ##############################################
 #             INSTALL SKIN RNSE              #
 ##############################################
-#if [ -e /boot/skin.rnse*.zip ]; then
+##### ##### if [ -e /boot/skin.rnse*.zip ]; then
 if [ -e /home/pi/skin.rnse*.zip ]; then
 	rm -r /home/pi/.kodi/addons/skin.*rns*
 	unzip /home/pi/skin.rnse*.zip -d /home/pi/.kodi/addons/ > /dev/null 2>&1
